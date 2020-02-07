7e6796f46b85031822ff5d24976c0bc1a4b8f6ec
Gal Elmalah <gale@wix.com>
2020-02-07 21:21:22 +0200

    some basic operations implemnted

---
+++ src/Matrix.js
@@ -0,0 +1,97 @@
+function defaultMapper(i,j) {
+  return 1
+}
+
+class Matrix {
+  constructor(n, m, options = {}){
+    this.rowsSize = n;
+    this.columnsSize = m;
+    this.options = options;
+    this._matrix = []
+    return this;
+  }
+
+  get size() {
+    return [this.rowsSize, this.columnsSize]
+  }
+
+  get matrix() {
+    if(this._matrix.length) {
+      return this._matrix;
+    }
+    return this.build()
+  }
+
+  build() {
+    const mapper = this.options.mapper || defaultMapper
+    for(let i=0; i < this.rowsSize ; i++) {
+      let column = [];
+      for(let j=0; j < this.columnsSize ; j++) {
+        column[j] = mapper(i,j)
+      }
+      this._matrix [i] = column;
+    }
+    return this._matrix ;
+  }
+
+  hasTheSameSize(matrixTwo) {
+    return this.size[0] === matrixTwo.size[0] && this.size[1] === matrixTwo.size[1]
+  }
+
+
+  cell(i,j) {
+    if(i < 0 || i > this.rowsSize -1|| j<0 || j > this.columnsSize-1) {
+      throw new RangeError('Illegal indexes')
+    }
+    return this.matrix[i][j]
+  }
+
+  plus(matrixTwo) {
+    if(!this.hasTheSameSize(matrixTwo)) {
+      throw new Error(`Mismatch matrix sizes.\ncannot add two matrixes of sizes: mat1 -> ${this.size} :::: mat2 -> ${matrixTwo.size}  `)
+    }
+    const [n,m] = this.size;
+    const mapper = (i,j) => this.cell(i,j) + matrixTwo.cell(i,j)
+    return new Matrix(n,m, {mapper})
+  }
+
+  minus(matrixTwo) {
+    if(!this.hasTheSameSize(matrixTwo)) {
+      throw new Error(`Mismatch matrix sizes.\ncannot add two matrixes of sizes: mat1 -> ${this.size} :::: mat2 -> ${matrixTwo.size}  `)
+    }
+    const [n,m] = this.size;
+    const mapper = (i,j) => this.cell(i,j) - matrixTwo.cell(i,j)
+    return new Matrix(n,m, {mapper})
+  }
+
+  map(fn) {
+    const [n,m] = this.size;
+    const mapper = (i,j) => fn(this.cell(i,j), [i,j], this.matrix)
+    return new Matrix(n,m, {mapper})
+  }
+
+  transpose() {
+
+    const [n,m] = this.size;
+    const mapper = (i,j) => this.cell(j,i)
+    return new Matrix(m,n, {mapper})
+  }
+
+  toString() {
+    let rows = []
+    for(let i=0; i < this.rowsSize ; i++) {
+      let row = '| '
+      for(let j=0; j < this.columnsSize ; j++) {
+        row+= this.cell(i,j) + ' ';
+      }
+      row+=' |'
+      rows.push(row)
+    }
+    console.log(rows.join('\n'))
+  }
+
+}
+
+
+
+module.exports.Matrix = Matrix;
--- src/Matrix.spec.js
+++ src/Matrix.spec.js
@@ -1,7 +1,95 @@
-const Matrix = require('./Matrix')
+const {Matrix} = require('./Matrix')
 
 describe('Matrix', () => {
   it('should build matrix correctly', () => {
-    
+    const rows = 3;
+    const column = 4;
+    const matrix = new Matrix(rows,column);
+    expect(matrix.size).toEqual([rows,column])
+    expect(matrix.build()).toEqual([[1,1,1,1],[1,1,1,1],[1,1,1,1]])
   })
+
+  it('should map the matrix correctly', () => {
+    const rows = 2;
+    const column = 2;
+    const matrix = new Matrix(rows,column, {mapper: (i,j) => `${i},${j}`});
+    expect(matrix.size).toEqual([rows,column])
+    expect(matrix.build()).toEqual([['0,0', '0,1'], ['1,0', '1,1']])
+  })
+})
+
+describe('Matrix operations', () => {
+
+  describe('Addition', () => {
+    it('should throw if matrixes are of different sizes when adding', () => {
+      const rows = 3;
+      const column = 4;
+      const matrixOne = new Matrix(rows +1,column);
+      const matrixTwo = new Matrix(rows,column);
+      expect(() => matrixOne.plus(matrixTwo)).toThrow(Error);
+    })
+  
+    it('should add matrixes', () => {
+      const rows = 2;
+      const column = 2;
+      const matrixOne = new Matrix(rows,column);
+      const matrixTwo = new Matrix(rows,column);
+      const additionResult = matrixOne.plus(matrixTwo);
+      expect(additionResult).not.toBe(matrixTwo);
+      expect(additionResult).not.toBe(matrixOne);
+      expect(additionResult.build()).toEqual([[2,2],[2,2]])
+    })
+  
+  })
+
+  describe('Subtractions' , () => {
+    it('should throw if matrixes are of different sizes when adding', () => {
+      const rows = 3;
+      const column = 4;
+      const matrixOne = new Matrix(rows +1,column);
+      const matrixTwo = new Matrix(rows,column);
+      expect(() => matrixOne.minus(matrixTwo)).toThrow(Error);
+    })
+
+    it('should subtract matrixes', () => {
+        const rows = 2;
+        const column = 2;
+        const matrixOne = new Matrix(rows,column);
+        const matrixTwo = new Matrix(rows,column);
+        const additionResult = matrixOne.minus(matrixTwo);
+        expect(additionResult).not.toBe(matrixTwo);
+        expect(additionResult).not.toBe(matrixOne);
+        expect(additionResult.build()).toEqual([[0,0],[0,0]])
+    })
+  })
+
+  describe('map' , () => {
+    it('should call the mapper function with the cell value, indexes and the matrix itself', () => {
+      const mock = jest.fn();
+      const originalMat = new Matrix(2,2);
+      const mat = originalMat.map(mock);
+      expect(mat).not.toBe(originalMat)
+      mat.build()
+      expect(mock).toHaveBeenCalledTimes(4)
+      expect(mock).toHaveBeenLastCalledWith(originalMat.cell(1,1),[1,1],expect.anything())
+    })
+
+    it('apply the mapping correctly', () => {
+      const fn = (value, [i,j]) => value + i + j;
+      const originalMat = new Matrix(2,2);
+      const mat = originalMat.map(fn);
+      expect(mat).not.toBe(originalMat)
+      expect(mat.build()).toEqual([[1, 2], [2, 3]])
+    })
+  })
+
+  describe('Transpose' , () => {
+    it('should transpose the matrix correctly', () => {
+      const mat = new Matrix(2,3, {mapper:(i,j) => i+j});
+      const transposed = mat.transpose();
+      expect(transposed).not.toBe(mat)
+      expect(transposed.build()).toEqual([[0,1],[1,2],[2,3]])
+    })
+  })
+
 })
